5bac1872a75e5b2f1b25663ac2edf5a4637211f0
Nicholas <dojoman19@gmail.com>
2018-01-20 00:04:58 -0500

    Made solution for unicode filtering a bit more elegant

--- src/SmsXmlToTxt.java
+++ src/SmsXmlToTxt.java
@@ -23,6 +23,32 @@ import java.util.Scanner;
 
 public class SmsXmlToTxt {
 
+    private static void deleteTroubleChars(ArrayList<String> copyLines, File xmlCopy) throws IOException{
+        //Iterate over each line in the file 
+        for(int i = 0; i < copyLines.size(); i++){
+            //Skip it if it's empty
+            String currentLine = copyLines.get(i);
+            if(currentLine.length() == 0){
+                continue;
+            }
+            //While it contains a problem string 
+            while(currentLine.contains(";&#")){
+                //Get rid of it 
+                int firstInd = currentLine.indexOf(";&#");
+                currentLine = currentLine.substring(0, firstInd-8) + currentLine.substring(firstInd+9);
+            }
+            //Copy the corrected line 
+            copyLines.set(i, currentLine);
+        }
+        
+        //Write each new line to the xmlCopy file for processing
+        BufferedWriter writer = new BufferedWriter(new FileWriter(xmlCopy));
+        for(String currLine : copyLines){
+            writer.write(currLine + "\n");
+        }
+        writer.close();
+    }
+
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
 
         //Get Specific Original File
@@ -34,56 +60,18 @@ public class SmsXmlToTxt {
         xmlCopy.createNewFile();
 		//Catch instance of no test input file
         try{
-			Files.copy(xmlIn.toPath(), xmlCopy.toPath(), StandardCopyOption.REPLACE_EXISTING);
-		}
-		catch(NoSuchFileException exep){
-			System.err.println("The file was not found: ./TestXML.xml");	
-			return;
-		}
+            Files.copy(xmlIn.toPath(), xmlCopy.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+        catch(NoSuchFileException exep){
+            System.err.println("File was not found: ./TestXML.xml");
+            return;
+        }
         System.out.println("Test");
-       
+        
         //Convert file to an ArrayList<String> containing each line
         ArrayList<String> copyLines = (ArrayList<String>)Files.readAllLines(xmlCopy.toPath());
-        //Iterate over each line in the file 
-        for(int i = 0; i < copyLines.size(); i++){
-            //Skip it if it's empty
-            String currentLine = copyLines.get(i);
-            if(currentLine.length() == 0){
-                continue;
-            }
-            String temp = null;
-            //If the string contains problematic unicode
-            if(currentLine.contains("&#")){
-                temp = "";
-                //Split by spaces, trimming leading whitespace 
-                String[] split = currentLine.trim().split(" ");
-                //Check for bad unicode in each token
-                for(int j = 0; j < split.length; j++){
-                    String tok = split[j];
-                    if(tok.charAt(0) == '&' && tok.charAt(1) == '#'){
-                        //If there's a quote, add that in and get rid of extra space
-                        if(tok.contains("\"")){
-                            temp = temp.substring(0, temp.length() - 1).concat("\" "); 
-                        }
-                    }
-                    //Otherwise, concatenate this token with a space (since we got rid of them)
-                    else{
-                        temp = temp.concat(tok) + " ";
-                    }
-                }
-            }
-            //If we ran into a problematic sequence, copy edited line
-            if(temp != null){
-                copyLines.set(i, "  " + temp);
-            }
-        }
-        
-        //Write each new line to the xmlCopy file for processing
-        BufferedWriter writer = new BufferedWriter(new FileWriter(xmlCopy));
-        for(String currLine : copyLines){
-            writer.write(currLine + "\n");
-        }
-        writer.close();
+        //Delete troublesome unicode characters 
+        deleteTroubleChars(copyLines, xmlCopy);
         
         //Use DOM model for parsing through XML File
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
